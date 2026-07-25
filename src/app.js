0f97d43037ea8bb23d1868b030f43db9c837c9db
unknown <chinarayudukatta@gmail.com>
2026-07-25 11:09:54 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req, res)=> {
+// req /user /user/xyz, /user/1
+app.get("/user/:userId/:name/:password", (req, res)=> {
+    console.log(req.params)
     res.send({
         firstName: "Chinna",
         lastName: "Rayudu"
     })
 })
 
-app.post("/user", (req, res)=> {
-    // saving data to db
-    res.send("Data successfully saved to the database")
-})
-
-app.delete("/user", (req, res)=> {
-    res.send("Deleted Successfully!")
-})
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res)=> {
-    res.send("Hello from server!")
-})
 
 app.listen(7777, ()=> {
     console.log("Server is successfully listen on port 3000....")
