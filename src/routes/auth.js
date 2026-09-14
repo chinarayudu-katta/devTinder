e075146027aaa0426de398476ffb2b57276b2a89
unknown <chinarayudukatta@gmail.com>
2026-09-14 06:05:14 +0530

    singup form updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,8 +18,14 @@ authRouter.post("/signup", async (req, res)=> {
             emailId,
             password: passwordHash,
         });    
-        await user.save();
-        res.send("User added successfully...")
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();     
+
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 1 * 3600000)
+        });
+
+        res.json({message: "User added successfully...", data: savedUser});
     } catch (err){
         res.status(400).send("ERROR: " + err.message)    
     }
