ef3e91fbbf184e6ea5a57160379d7a4ffda157b5
unknown <chinarayudukatta@gmail.com>
2026-09-14 10:33:04 +0530

    updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(
-    "mongodb+srv://chinarayudukatta_db_user:GmZwlUob0zbsDKQC@hellonode.dhm5vpj.mongodb.net/devTinder"
+    "mongodb+srv://chinarayudukatta_db_user:G3JovEdZt0Outq6S@hellonode.dhm5vpj.mongodb.net/devTinder"
 )
 }
 module.exports = connectDB;
